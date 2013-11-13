c043dc7c1661c092392c1667f5def94d5cd859b1
Ivan Nikolić <niksy5@gmail.com>
2013-11-14 00:35:05 +0100

    Release 2.3.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,14 +29,35 @@ module.exports = function (grunt) {
 					'picture.min.js': ['picture.js']
 				}
 			}
+		},
+
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: ['pkg'],
+				commit: true,
+				commitMessage: 'Release %VERSION%',
+				commitFiles: ['-a'], // '-a' for all files
+				createTag: true,
+				tagName: '%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: false,
+				pushTo: 'upstream',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 
 	});
 
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-bump' );
 
 	grunt.registerTask( 'default', ['concat:dist', 'uglify:dist'] );
+	grunt.registerTask( 'releasePatch', ['bump-only:patch', 'default', 'bump-commit'] );
+	grunt.registerTask( 'releaseMinor', ['bump-only:minor', 'default', 'bump-commit'] );
+	grunt.registerTask( 'releaseMajor', ['bump-only:major', 'default', 'bump-commit'] );
+
 
 };
 
